e9890f6b079acedb2e5e170ceb8525086b1b9231
useDuanly <use.d.v.a.dev@gmail.com>
2022-12-03 20:25:58 +0000

    fin

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,10 +3,12 @@ const apiData = fetchData("https://dog.ceo/api/breeds/image/random/50");
 
 const Gallery = () => {
   const data = apiData.read();
+  const amountInitial = 0;
+  const amount = 15;
 
   return (
     <div className="gallery">
-      {data.map((item, index) => (
+      {data.slice(amountInitial, amount).map((item, index) => (
         <img
           alt={`gallery-img-${index + 1}`}
           src={item}
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,13 @@
 function Logo() {
   return (
     <div className="logo">
-      <img alt="logo" src="https://dog.ceo/img/dog-api-logo.svg" />
+      <img
+        alt="logo"
+        src={
+          "https://dog.ceo/img/dog-api-logo.svg" ||
+          "https://placeimg.com/250/250/arch/grayscale"
+        }
+      />
     </div>
   );
 }
